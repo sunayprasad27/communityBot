12304b983f8b3d4a94d62dfa2f5b76c056da97c1
Sunay Prasad RK <M1026723@mindtree.com>
2016-08-09 20:34:13 +0530

    Updated code

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ server.listen(process.env.PORT || 3000, function()
    console.log('%s listening to %s', server.name, server.url); 
 });
 
+var appId = process.env.MICROSOFT_APP_ID || "Missing your App Id";
+var appPassword = process.env.MICROSOFT_APP_PASSWORD || "Missing your App Password";
+
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'e6f7d81b-7639-4525-ace6-2986d4ed8901', appPassword: 'yd0eoLEBFobyWhVhWnQYaH7' }); 
+({ appId: process.env.MICROSOFT_APP_ID, appPassword: process.env.MICROSOFT_APP_PASSWORD }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
@@ -19,3 +22,8 @@ server.post('/api/messages', connector.listen());
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
+
+server.get('/', restify.serveStatic({
+ directory: 'D:\PROJECTS\ChatBot\CommunityBot\communityBot',
+ default: '/index.html'
+}));
